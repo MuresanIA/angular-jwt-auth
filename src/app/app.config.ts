1d667da8f12302eb05ec38c253d33f0e4597c4cc
alexandru.muresan <alexandru.muresan933@gmail.com>
2024-07-04 14:30:17 +0300

    first push

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,17 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { environment } from '../environments/environment.development';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth())]
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyDA6bcMu9LozZaF9taGAJq9OVdhSXY7tdY",
+        authDomain: "jwt-auth-ecbda.firebaseapp.com",
+        projectId: "jwt-auth-ecbda",
+        storageBucket: "jwt-auth-ecbda.appspot.com",
+        messagingSenderId: "742096953330",
+        appId: "1:742096953330:web:9df005585fa345532dd56b",
+        measurementId: "G-Q5X9C7FQL5"
+    }
+};
